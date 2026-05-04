a418ca5d46096a7b2ca36d3ae7c88411751c02fd
codingapptzf-star <codingapptzf@gmail.com>
2026-05-04 21:20:04 +0700

    Update Supabase URL in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ⚠️ GANTI DUA BARIS INI DENGAN DATA SUPABASE ANDA
-const SUPABASE_URL = 'https://ogrboadxaeymeqsksdau.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://ogrboadxaeymeqsksdau.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ncmJvYWR4YWV5bWVxc2tzZGF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NzczNDAsImV4cCI6MjA5MTQ1MzM0MH0.liLIar3JdLuYq2ni0ANKHL4z0MJ8mP01MVS9eEt3EoU';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
